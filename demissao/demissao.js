9ba0b7362a6f8cc4ab663627cbc0ed53507e8a93
marllonsousa <marllon.sousa@interconstrutora.com.br>
2022-07-03 12:21:41 -0300

    Alteração STYLE.CSS

--- demissao/demissao.js
+++ demissao/demissao.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-    $("#div_rd_Dslg,#div_anx_demissao,#div_rd_tpDslg,#div_txt_acrdPts,#div_rd_eftv").hide()
+    $("#div_rd_Dslg,#div_anx_demissao,#div_rd_tpDslg,#div_txt_acrdPts,#div_rd_eftv,#recisao_colab").hide()
     if (FM == "VIEW" || FM == "MOD") {
         $("span.form-control").removeAttr("style")
     }
-    // if (FM == "ADD" || FM == "MOD") {
-    //     // $(".select2").select2()
-    //     // Carregar SPE
-    //     $.ajax({
-    //         type: "GET",
-    //         url: "https://experterp.com.br/inter/apiFluig/getSPE.php",
-    //         dataType: "json",
-    //         success: function (response) {
-    //             $.each(response.data, function (index, value) {
-    //                 $("<option></option>", {
-    //                     value: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME,
-    //                     text: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME
-    //                 }).appendTo("#slt_spe")
-    //             })
-    //         },
-    //         error: function () {
-    //             $.each(response.data, function (index, value) {
-    //                 $("<option></option>", {
-    //                     value: "Erro ao carregar SPEs",
-    //                     text: "Erro ao carregar SPEs"
-    //                 }).appendTo("#slt_spe")
-    //             })
-    //         }
-    //     })
-    // }
+    if (FM == "ADD" || FM == "MOD") {
+        // $(".select2").select2()
+        // Carregar SPE
+        $.ajax({
+            type: "GET",
+            url: "https://experterp.com.br/inter/apiFluig/getSPE.php",
+            dataType: "json",
+            success: function (response) {
+                $.each(response.data, function (index, value) {
+                    $("<option></option>", {
+                        value: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME,
+                        text: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME
+                    }).appendTo("#slt_spe")
+                })
+            },
+            error: function () {
+                $.each(response.data, function (index, value) {
+                    $("<option></option>", {
+                        value: "Erro ao carregar SPEs",
+                        text: "Erro ao carregar SPEs"
+                    }).appendTo("#slt_spe")
+                })
+            }
+        })
+    }
     if (ATV >= 0 || ATV == "null") {
         var rd_Estg_clc
         $("input[name$='rd_Estg']").click(function (e) {
@@ -64,6 +64,13 @@ $(document).ready(function() {
                 $("#div_txt_acrdPts").show()
             }else if($(this).val() == "Não"){
                 $("#div_txt_acrdPts").hide()
+            }
+        })
+        $("input[name$='rd_anx_dp_ok']").click(function (e) {
+            if($(this).val() == "Sim"){
+                $("#recisao_colab").show()
+            }else if($(this).val() == "Não"){
+                $("#recisao_colab").hide()
             }
         })
     }
