198098b0aafe74796dffa2f52c8bff33de101b18
marllonsousa <marllon.sousa@interconstrutora.com.br>
2022-07-03 11:48:38 -0300

    Teste ok

--- geral/upload.js
+++ geral/upload.js
@@ -10,10 +10,6 @@
  * @return {void} 
  * @author Sérgio Machado
  */
-
-$(document).ready(function () {
-    alert("Git Ok")
-});
 function anexo(event) {
     try {
         const acao = event.currentTarget.getAttribute("data-acao");
@@ -287,7 +283,6 @@ function btnState(idInput, acao, btn) {
  * @author Sérgio Machado
  */
 function displayBtnFiles() {
-    alert("Ok")
     try {
         $('.componentAnexo').each(function (i, element) {
             let inputFile = $(element).find(".inputAnexo")
