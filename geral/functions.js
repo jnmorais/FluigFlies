4532af73ba7804d59c6a65000b68ba92430a7636
marllonsousa <marllon.sousa@interconstrutora.com.br>
2022-07-03 18:05:54 -0300

    a

--- geral/functions.js
+++ geral/functions.js
@@ -1,7 +1,7 @@
 // Ação ao clicar no botão documento
 function viewDoc() {
-    var url = "https://interconstrutora136393.fluig.cloudtotvs.com.br/portal/p/1/ecmnavigation?app_ecm_navigation_doc=";
-    parent.open(url + documento);
+    var url = "https://interconstrutora136608.fluig.cloudtotvs.com.br/portal/p/1/ecmnavigation?app_ecm_navigation_doc=";
+    parent.open(url + $("#documento").val());
 }
 function visualizarSolicitacao() {
     var url = "https://interconstrutora136608.fluig.cloudtotvs.com.br/portal/p/1/pageworkflowview?app_ecm_workflowview_processInstanceId="
