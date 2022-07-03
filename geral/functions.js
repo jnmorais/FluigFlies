f3581e1db046fb6f825a14f9f15b82b00070bcd4
marllonsousa <marllon.sousa@interconstrutora.com.br>
2022-07-03 19:11:56 -0300

    Update functions.js

--- geral/functions.js
+++ geral/functions.js
@@ -1,9 +1,32 @@
 // Ação ao clicar no botão documento
+$(document).ready(function () {
+    displayBtnFiles()
+    $(".copyCode").click(function (e) {
+        e.preventDefault()
+        navigator.clipboard.writeText($(this).val())
+        FLUIGC.toast({
+            title: '',
+            message: 'O Código ' + $(this).val() + ' foi copiado!',
+            type: 'success'
+        });
+    })
+})
 function viewDoc() {
-    var url = "https://interconstrutora136608.fluig.cloudtotvs.com.br/portal/p/1/ecmnavigation?app_ecm_navigation_doc=";
+    var url = "https://interconstrutora136393.fluig.cloudtotvs.com.br/portal/p/1/ecmnavigation?app_ecm_navigation_doc=";
     parent.open(url + $("#documento").val());
 }
 function visualizarSolicitacao() {
-    var url = "https://interconstrutora136608.fluig.cloudtotvs.com.br/portal/p/1/pageworkflowview?app_ecm_workflowview_processInstanceId="
-    parent.open(url + solicitacao);
+    var url = "https://interconstrutora136393.fluig.cloudtotvs.com.br/portal/p/1/pageworkflowview?app_ecm_workflowview_processInstanceId="
+    parent.open(url + $("#solicitacao").val());
+}
+function anexarFile(nome, input) {
+    var btn = parent.document.getElementById("ecm-navigation-inputFile-clone");
+    if (btn && document.createEvent) {
+        btn.setAttribute("data-on-camera", "true");
+        if (nome && input) {
+            btn.setAttribute("data-file-name-camera", nome)
+            btn.setAttribute("data-inputNameFile", input)
+        }
+        btn.click();
+    }
 }
