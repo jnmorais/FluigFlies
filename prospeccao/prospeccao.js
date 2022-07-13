f948653754233372be517ff1d38804179210c4d7
Marllon Sousa <marllon.sousa@interconstrutora.com.br>
2022-07-13 14:03:47 -0300

    *

--- prospeccao/prospeccao.js
+++ prospeccao/prospeccao.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $("input[name='txt_vlrTpg']").mask('000.000.000.000.000,00', {reverse: true});
+    
     // AQUI CONTROLA A EXIBICAO EM QUALQUER MOMENTO DO FORMULARIO DOS BOTOES QUE JA FORAM CLICADOS
     if (ATV == null || FM == "VIEW" || FM == "MOD" || FM == "ADD") {
         $("#nn_motivorepro, #nn_motivo1, #altadirecao_mot, #nn_minutareproprosp, #nn_minutareprodire,#EstMassa_aprv,#viab_Aprov,#orcOk,#ans_OrcViab_appv").hide();
