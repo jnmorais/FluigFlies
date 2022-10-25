08751bb3bb9d608f3f9ff00d7ff3a89271a2aa6e
Marllon Sousa <102380447+marllonsousa@users.noreply.github.com>
2022-10-25 14:56:24 -0300

    Update transferencia.js

--- transferencia/transferencia.js
+++ transferencia/transferencia.js
@@ -46,21 +46,20 @@ $(document).ready(function () {
         }
     });
     // Ocultar campos
-    $("#div_end_atual, #div_end_destino, #div_nv_salario, #div_carga_hr, #div_nv_cargo").hide()
+    // $("#div_end_atual, #div_end_destino, #div_nv_salario, #div_carga_hr, #div_nv_cargo").hide()
+    $("#div_nv_salario","#div_nv_cargo","#div_end_atual","#div_end_destino","#div_alt_cgH","#div_carga_hr").hide()
     var processo_num, subprocesso;
     if ($("input[name$='processo_num']").val() != "" && $("input[name$='processo_num']").val() != undefined && $("input[name$='subprocesso']").val() != "" && $("input[name$='subprocesso']").val() != undefined) {
         processo_num = $("input[name$='processo_num']").val()
         subprocesso = $("input[name$='subprocesso']").val()
         $("#msg_processo").append("<strong style='color:#fff;text-align:right;'> - Origem: " + subprocesso + " Nº: " + processo_num + "</strong>")
     }
-
     show_on_click("rd_locA", "div_end_atual", "Obra")
     show_on_click("rd_locD", "div_end_destino", "Obra")
     show_on_click("rd_alt_sal", "div_nv_salario", "Sim")
     show_on_click("rd_alt_cargo", "div_nv_cargo", "Sim")
     show_on_click("rd_alt_cgH", "div_carga_hr", "Sim")
     show_on_click("rd_dp_infos", "div_dp_info", "Sim")
-
     show_on_load("rd_locA", "div_end_atual", "Obra")
     show_on_load("rd_locD", "div_end_destino", "Obra")
     show_on_load("rd_alt_sal", "div_nv_salario", "Sim")
@@ -68,7 +67,6 @@ $(document).ready(function () {
     show_on_load("rd_alt_cgH", "div_carga_hr", "Sim")
     show_on_load("rd_dp_infos", "div_dp_info", "Sim")
 });
-
 // Controla a exibição ao clicar
 function show_on_click(campo, div, valor) {
     $("input[name$='" + campo + "']").click(function () {
