b084b26faa7e47c1283ef84fcc6d5200c93d979f
Marllon Sousa <102380447+marllonsousa@users.noreply.github.com>
2022-11-08 13:52:39 -0300

    Update aberturaspe.js

--- aberturaspe/aberturaspe.js
+++ aberturaspe/aberturaspe.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
     // Controla os cliques nos campos
     if (ATV == "null" || FM == "VIEW" || FM == "MOD" || FM == "ADD") {
-        $(".select2").select2()
         $("#div_jur_just, #div_fis_just").hide()
     }
-    //FORMULÁRIO INICIAL:
-    $("input[name$='jur_analise']").click(function () {
-        // alert()
-        if ($(this).val() == "Sim") {
-            $("#div_jur_just").show()
-        } else {
-            $("#div_jur_just").hide()
-        }
-        // $(this).val() == "Sim" ? $("#div_jur_just").show() : $("#div_jur_just").hide()
-    });
-    //FISCAL:
-    $("input[name$='fis_analise']").click(function () {
-        // alert()
-        if ($(this).val() == "Sim") {
-            $("#div_fis_just").show()
-        } else {
-            $("#div_fis_just").hide()
-        }
-    });
+    exibircampoClick("jur_analise","Sim",null,"div_jur_just")
+    exibircampoClick("fis_analise","Sim",null,"div_fis_just")
+    
     exibircampo("fis_analise", "Sim", "div_fis_just")
     exibircampo("jur_analise", "Sim", "div_jur_just")
 });
 function exibircampo(campo, valor, div) {
     $("input[name$='" + campo + "']:checked").val() == valor ? $("#" + div).show() : $("#" + div).hide()
+}
+function exibircampoClick(campo, valor1, valor2, show) {
+    $("[name$='" + campo + "']").click(function () {
+        if (valor2 == null) {
+            if ($(this).val() == valor1) {
+                $("#" + show).show();
+            } else {
+                $("#" + show).hide();
+            }
+        } else {
+            if ($(this).val() == valor1 || $(this).val() == valor2) {
+                $("#" + show).show();
+            } else {
+                $("#" + show).hide();
+            }
+        }
+    });
 }
