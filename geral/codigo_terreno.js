2deef0f2338cce62ae4741e70ae3a1e7711caa05
Marllon Sousa <102380447+marllonsousa@users.noreply.github.com>
2022-11-08 14:31:05 -0300

    Create codigo_terreno.js

---
+++ geral/codigo_terreno.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    let url_prod = "https://interconstrutora136393.fluig.cloudtotvs.com.br/api/public/ecm/dataset/search?datasetId=009"
+    let url_homg = "https://interconstrutora136608.fluig.cloudtotvs.com.br/api/public/ecm/dataset/search?datasetId=ds_consultaprospeccao"
+    let url_base = window.location.href
+    let url_dataset = ""
+
+    if (url_base.match("136393")) {
+        url_dataset = url_prod
+    } else {
+        url_dataset = url_homg
+    }
+    $("#slt_terreno").change(function (e) {
+        let nmclTerreno = $(this).val()
+        e.preventDefault();
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: url_dataset,
+            success: function (response) {
+                let val = response.content.find(o => o.nmclTerreno == nmclTerreno)
+                if (val) {
+                    $("#txt_enderecoTrr").val(val.endereco)
+                } else {
+                    FLUIGC.toast({ title: 'Erro ao obter terreno', message: 'Não foi possível obter os dados do terreno selecionado!', type: 'danger' });
+                }
+            }
+        });
+    });
+});
