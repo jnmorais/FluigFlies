ea3b93d946065a3056220801787900fcf1e045f9
Marllon Sousa <102380447+marllonsousa@users.noreply.github.com>
2022-11-09 13:16:59 -0300

    Update transferencia.js

--- transferencia/transferencia.js
+++ transferencia/transferencia.js
@@ -1,51 +1,8 @@
 $(document).ready(function () {
-    $(".select2").select2();
-    $.ajax({
-        type: "GET",
-        url: "https://experterp.com.br/inter/apiFluig/getCentroCusto.php",
-        dataType: "json",
-        success: function (response) {
-            $.each(response.data, function (index) {
-                $("<option></option>", {
-                    value: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME,
-                    text: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME
-                }).appendTo("#slt_cc_atual,#slt_cc_destino");
-            });
-        },
-        error: function () {
-            $.each(response.data, function () {
-                $("<option></option>", {
-                    value: "Erro ao carregar centros de custo",
-                    text: "Erro ao carregar centros de custo"
-                }).appendTo("#slt_cc_atual,#slt_cc_destino");
-            });
-        }
-    });
-    $.ajax({
-        type: "GET",
-        url: "https://experterp.com.br/inter/apiFluig/getSPE.php",
-        dataType: "json",
-        success: function (response) {
-            $.each(response.data, function (index) {
-                $("<option></option>", {
-                    value: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME,
-                    text: "(" + response.data[index].CODIGO + ") - " + response.data[index].NOME
-                }).appendTo("#slt_spe_atual,#slt_spe_destino");
-            });
-        },
-        error: function () {
-            $.each(response.data, function () {
-                $("<option></option>", {
-                    value: "Erro ao carregar SPEs",
-                    text: "Erro ao carregar SPEs"
-                }).appendTo("#slt_spe_atual,#slt_spe_destino");
-            });
-        }
-    });
+    let cc_atual, cc_destino, processo_num, subprocesso
     // Ocultar campos
     // #div_dp_info
-    $("#div_end_atual, #div_end_destino, #div_nv_salario, #div_carga_hr, #div_nv_cargo").hide()
-    var processo_num, subprocesso;
+    $("#div_end_atual, #div_end_destino, #div_nv_salario, #div_carga_hr, #div_nv_cargo,#apv_diretor").hide()
     if ($("input[name$='processo_num']").val() != "" && $("input[name$='processo_num']").val() != undefined && $("input[name$='subprocesso']").val() != "" && $("input[name$='subprocesso']").val() != undefined) {
         processo_num = $("input[name$='processo_num']").val()
         subprocesso = $("input[name$='subprocesso']").val()
@@ -73,4 +30,11 @@ function show_on_click(campo, div, valor) {
 // Controla a exibição dos campos ocultos
 function show_on_load(campo, div, valor) {
     $("input[name$='" + campo + "']:checked").val() == valor ? $("#" + div).show() : $("#" + div).hide()
+}
+function setSelectedZoomItem(selectedItem) {
+    selectedItem.inputId == "cc_atual" ? cc_atual = selectedItem["CC"] : false
+    selectedItem.inputId == "cc_destino" ? cc_destino = selectedItem["CC"] : false
+    if (cc_atual == cc_destino) {
+        $("#apv_diretor").show()
+    }
 }
