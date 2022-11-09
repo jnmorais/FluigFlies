4140370a30f47d4682b44dde00654a2fac4c7458
Marllon Sousa <102380447+marllonsousa@users.noreply.github.com>
2022-11-09 07:57:50 -0300

    Update distrato.js

--- distrato/distrato.js
+++ distrato/distrato.js
@@ -1,55 +1,68 @@
 // Controla exibição na atividade 1
-$(document).ready(function() {
+$(document).ready(function () {
     // Controla os cliques nos campos
-    if(FM == "MOD" || FM == "ADD"){
+    if (FM == "MOD" || FM == "ADD") {
+        $("input[name$='rd_terreno']").click(function () {
+            if ($(this).val() == "Sim") {
+                $("#div_slt_terreno,#div_txt_solicitacao").show()
+                $("#txt_readonly input").attr("readonly", true)
+                $("#txt_readonly input").val("")
+                $("#txt_readonly input").text("")
+                $("[attr-class='mudarClasse']").removeClass("col-md-4 form-group").addClass("col-md-3 form-group");
+            } else {
+                $("#div_slt_terreno,#div_txt_solicitacao").hide()
+                $("#txt_readonly input").attr("readonly", false)
+                $("#txt_readonly input").val("")
+                $("#txt_readonly input").text("")
+                $("[attr-class='mudarClasse']").removeClass("col-md-3 form-group").addClass("col-md-4 form-group");
+            }
+        });
         // INICIO
-        controlaClick("rd_terreno","Sim","terr_ext")
         // (Direção) Análise do Distrato
-        controlaClick("rd_distrato","Não","slt_att_infos")
+        controlaClick("rd_distrato", "Não", "slt_att_infos")
         // Elaboração do Distrato
-        controlaClick("rd_nvsInfos","Sim","nvsInfos_elb_dst")
-        controlaClick("rd_nvsInfos","Não","anx_elb_dst")
+        controlaClick("rd_nvsInfos", "Sim", "nvsInfos_elb_dst")
+        controlaClick("rd_nvsInfos", "Não", "anx_elb_dst")
         // (Direção) Validação do Distrato
-        controlaClick("rd_dst_dir_apv","Não","mtv_dst_dir_rpv")
+        controlaClick("rd_dst_dir_apv", "Não", "mtv_dst_dir_rpv")
         // Assinatura do distrato
-        controlaClick("rd_ass_dst","Não","mtv_ass_dst")
-        controlaClick("rd_ass_dst","Sim","anx_ctt_ass")
+        controlaClick("rd_ass_dst", "Não", "mtv_ass_dst")
+        controlaClick("rd_ass_dst", "Sim", "anx_ctt_ass")
         //Validação Prospector 
         controlaClick("rd_dst_prp_apv", "Não", "mtv_dst_prp_rpv")
         //Aditivo 
         controlaClick("rd_ass_dst", "Iniciar aditivo", "aditivo")
-     
+
     }
     // Controla os campos que já foram clicados anteriormente
-    if(FM == "MOD" || FM == "VIEW" || ATV == "null"){
+    if (FM == "MOD" || FM == "VIEW" || ATV == "null") {
         // INICIO
-        controlaExb("rd_terreno","Sim","terr_ext")
         // (Direção) Análise do Distrato
-        controlaExb("rd_distrato","Não","slt_att_infos")
+        controlaExb("rd_distrato", "Não", "slt_att_infos")
         // Elaboração do Distrato
-        controlaExb("rd_nvsInfos","Sim","nvsInfos_elb_dst")
-        controlaExb("rd_nvsInfos","Não","anx_elb_dst")
+        controlaExb("rd_nvsInfos", "Sim", "nvsInfos_elb_dst")
+        controlaExb("rd_nvsInfos", "Não", "anx_elb_dst")
         // (Direção) Validação do Distrato
-        controlaExb("rd_dst_dir_apv","Não","mtv_dst_dir_rpv")
+        controlaExb("rd_dst_dir_apv", "Não", "mtv_dst_dir_rpv")
         // Assinatura do distrato
-        controlaExb("rd_ass_dst","Não","mtv_ass_dst")
-        controlaExb("rd_ass_dst","Sim","anx_ctt_ass")
+        controlaExb("rd_ass_dst", "Não", "mtv_ass_dst")
+        controlaExb("rd_ass_dst", "Sim", "anx_ctt_ass")
         //Validação Prospector
         controlaExb("rd_dst_prp_apv", "Não", "mtv_dst_prp_rpv")
 
     }
     // Pega o valor das textareas das atvs att info e novas infos
-    if(ATV == 58 || ATV == 63){
-        
+    if (ATV == 58 || ATV == 63) {
+
     }
     // Controla os botoes de anexo // Caso estejam em modo leitura, vincula um click para a aba anexos e altera o texto para visualizr anexos
-    var anexos = ["anx_distrato","anx_att_infos","anx_elb_dst","anx_nvs_infos","anx_dst_rpv","anx_ctt_ass","anx_ntf_extj"]
+    var anexos = ["anx_distrato", "anx_att_infos", "anx_elb_dst", "anx_nvs_infos", "anx_dst_rpv", "anx_ctt_ass", "anx_ntf_extj"]
     for (var pos = 0; pos < anexos.length; pos++) {
-        if($("input[name*='"+anexos[pos]+"'").attr('readonly') == "readonly"){
-            $("input[name*='"+anexos[pos]+"'").attr('onclick','').unbind('click');
-            $("input[name*='"+anexos[pos]+"'").css('pointer-events','all');
-            $("input[name*='"+anexos[pos]+"'").val('Visualizar anexos');
-            $("input[name*='"+anexos[pos]+"'").on('click', function () {
+        if ($("input[name*='" + anexos[pos] + "'").attr('readonly') == "readonly") {
+            $("input[name*='" + anexos[pos] + "'").attr('onclick', '').unbind('click');
+            $("input[name*='" + anexos[pos] + "'").css('pointer-events', 'all');
+            $("input[name*='" + anexos[pos] + "'").val('Visualizar anexos');
+            $("input[name*='" + anexos[pos] + "'").on('click', function () {
                 $(window).scrollTop(0);
                 window.parent.document.querySelector("#tab-attachments > div > div.col-xs-8").click()
             });
@@ -57,22 +70,22 @@ $(document).ready(function() {
     }
     // Controla exibição dos titulos da tabela de pagamentos
     $("[thc='tpg']").hide()
-    $(".tpgBtn").click(function (e) {  e.preventDefault(); $("[thc='tpg']").show() })
-    if(ATV != 34)  $("[thc='tpg']").show()
+    $(".tpgBtn").click(function (e) { e.preventDefault(); $("[thc='tpg']").show() })
+    if (ATV != 34) $("[thc='tpg']").show()
 });
-function controlaExb(campo,v_campo,id_show){
-    if($("input[name$='"+campo+"']:checked").val() == v_campo){
-        $("#"+id_show).show()
-    }else{
-        $("#"+id_show).hide()
+function controlaExb(campo, v_campo, id_show) {
+    if ($("input[name$='" + campo + "']:checked").val() == v_campo) {
+        $("#" + id_show).show()
+    } else {
+        $("#" + id_show).hide()
     }
 }
-function controlaClick(campo,v_campo,id_show){
-    $("input[name$='"+campo+"']").click(function() {
+function controlaClick(campo, v_campo, id_show) {
+    $("input[name$='" + campo + "']").click(function () {
         if ($(this).val() == v_campo) {
-            $("#"+id_show).show()
-        }else{
-            $("#"+id_show).hide()
+            $("#" + id_show).show()
+        } else {
+            $("#" + id_show).hide()
         }
     });
 }
