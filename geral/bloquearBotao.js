fc0554fde650f63c8a08ade3dfece5d0a1e37884
Marllon Sousa <102380447+marllonsousa@users.noreply.github.com>
2022-12-14 11:09:18 -0300

    *

---
+++ geral/bloquearBotao.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    window.parent.$("#workflow-detail-card > div > div > button:nth-child(1)").prop("display: none;")
+    window.parent.$("#optionList > li:nth-child(2) > a").prop("display: none;")
+});
