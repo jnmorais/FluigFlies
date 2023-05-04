fbd0d6c43baaabd71977852991d12a63e7f52e59
Marllon Sousa <102380447+marllonsousa@users.noreply.github.com>
2023-05-04 16:04:39 -0300

    Update admissaoObra.js
    a

--- admissaoobra/admissaoObra.js
+++ admissaoobra/admissaoObra.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
                 }).on('dblclick', function (ev) {
                     var index = thisTable.selectedRows()[0];
                     var selected = thisTable.getRow(index);
-                    if (selected.vrf == "OBRA") {
+                    if (selected.vrf == "Obra") {
                         FLUIGC.toast({
                             title: 'Atenção: ',
                             message: 'Este cargo não é destinado a este processo!',
